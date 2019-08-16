8abf65b4f9b93eb8a9ca5638ecaacb22f4f1aeb8
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-16 11:07:39 -0300

    MIGRATIONS WORKING !!! IMPORTANT !!!

--- migrations/20190816102028_create_table_categories.js
+++ migrations/20190816102028_create_table_categories.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('categories', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.integer('parentId').unsigned().references('id').inTable('categories') //important to use unsigned for foreign keys
+
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('categories')
 };
--- migrations/20190816102053_create_table_articles.js
+++ migrations/20190816102053_create_table_articles.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('articles', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('description', 1000).notNull()
+        table.string('imageUrl', 1000)
+        table.binary('content').notNull()
+        table.integer('userId').unsigned().references('id').inTable('users').notNull() // important to use unsigned to foreign keys
+        table.integer('categoryId').unsigned().references('id').inTable('categories').notNull() //important to use unsigned to foreign keys
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('articles')
 };
