a9751be740537f7e29bc1ea0de9ba9ea3c9769cc
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-16 10:13:21 -0300

    Adding knex to connect with db

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    database: 'backend-study',
+    user: 'root',
+    password: '123456'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
