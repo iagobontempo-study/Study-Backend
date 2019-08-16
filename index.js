8e265fc533e46d62c3f0895eec2b3357703151ed
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-16 10:06:53 -0300

    Basic start

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign() // Inject middlewares inside application
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
 app.listen(3333, () => {
     console.log('Backend executando...')
