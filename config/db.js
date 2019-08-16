8fd802a7237cf3dbcad61e37233e29a75a834b77
Iago Bontempo <iagobontempo@hotmail.com>
2019-08-16 11:14:00 -0300

    Adding a first run option

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const config = require('../knexfile.js')
 const knex = require('knex')(config)
 
+//knex.migrate.latest([config]) // USE THIS TO INITIALIZE FIRST TIME, TO MAKE MIGRATIONS RUN INTO MYSQL
 module.exports = knex
